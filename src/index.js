3c6a65e5f78835c887c380dd6f307c82b2358de8
Aaron <ayybuddyy@gmail.com>
2019-10-10 17:59:48 -0400

    Feat: Project, responsive navbar
    Add in Natours project to projects section. Add responsive navbar
    for mobile layouts.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./sass/main.scss";
+
+const toggleButton = document.getElementsByClassName("navigation__toggle")[0];
+const navbarLinks = document.getElementsByClassName("navigation__container")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
