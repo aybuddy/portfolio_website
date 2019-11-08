b6a336322ad726f4a6e5d9843c1959989ca6d522
Aaron <ayybuddyy@gmail.com>
2019-11-07 23:31:01 -0500

    Fix smooth scroll bug

--- src/index.js
+++ src/index.js
@@ -11,48 +11,30 @@ document.addEventListener("click", evt => {
 
 // SmoothScroll
 
-function smoothScroll(target, duration) {
-  var target = document.querySelector(target);
-  var targetPosition = target.getBoundingClientRect().top;
-  var startPosition = window.pageYOffset;
-  var distance = targetPosition - startPosition;
-  var startTime = null;
-
-  function animationScroll(currentTime) {
-    if (startTime === null) startTime = currentTime;
-    var timeElapsed = currentTime - startTime;
-    var run = ease(timeElapsed, startPosition, distance, duration);
-    window.scrollTo(0, run);
-    if (timeElapsed < duration) requestAnimationFrame(animationScroll);
-  }
-
-  function ease(t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return (c / 2) * t * t + b;
-    t--;
-    return (-c / 2) * (t * (t - 2) - 1) + b;
-  }
-
-  requestAnimationFrame(animationScroll);
-}
-
-var heroSection = document.querySelector(".navigation__brand");
-heroSection.addEventListener("click", () => {
-  smoothScroll("#hero", 1000);
+window.scroll({
+  top: 2500,
+  left: 0,
+  behavior: "smooth"
 });
-var projectsSection = document.querySelector(".project__link");
-projectsSection.addEventListener("click", () => {
-  smoothScroll("#projects", 1000);
+
+window.scrollBy({
+  top: 100,
+  left: 0,
+  behavior: "smooth"
 });
-var aboutSection = document.querySelector(".about__link");
-aboutSection.addEventListener("click", () => {
-  smoothScroll("#about", 1000);
+
+document.querySelector("#hero").scrollIntoView({
+  behavior: "smooth"
 });
-var skillsSection = document.querySelector(".skills__link");
-skillsSection.addEventListener("click", () => {
-  smoothScroll("#skills", 1000);
+document.querySelector("#projects").scrollIntoView({
+  behavior: "smooth"
 });
-var contactSection = document.querySelector(".contact__link");
-contactSection.addEventListener("click", () => {
-  smoothScroll("#contact", 1000);
+document.querySelector("#about").scrollIntoView({
+  behavior: "smooth"
+});
+document.querySelector("#skills").scrollIntoView({
+  behavior: "smooth"
+});
+document.querySelector("#contact").scrollIntoView({
+  behavior: "smooth"
 });
