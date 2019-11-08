789342d1a0125fb10e430bc84fcc7fba56d78e9f
Aaron <ayybuddyy@gmail.com>
2019-11-08 01:24:53 -0500

    Try different method to fix nav bug

--- src/index.js
+++ src/index.js
@@ -5,43 +5,16 @@ import "./sass/main.scss";
 const toggleButton = document.getElementsByClassName("navigation__toggle")[0];
 const navbarLinks = document.getElementsByClassName("navigation__container")[0];
 
-document.addEventListener("click", evt => {
-  if (evt.target === toggleButton) navbarLinks.classList.toggle("active");
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
 });
 
 // SmoothScroll
 
-// window.scroll({
-//   top: 2500,
-//   left: 0,
-//   behavior: "smooth"
-// });
-
-// window.scrollBy({
-//   top: 100,
-//   left: 0,
-//   behavior: "smooth"
-// });
-
-// document.querySelector("#hero").scrollIntoView({
-//   behavior: "smooth"
-// });
-// document.querySelector("#projects").scrollIntoView({
-//   behavior: "smooth"
-// });
-// document.querySelector("#about").scrollIntoView({
-//   behavior: "smooth"
-// });
-// document.querySelector("#skills").scrollIntoView({
-//   behavior: "smooth"
-// });
-// document.querySelector("#contact").scrollIntoView({
-//   behavior: "smooth"
-// });
-
-(function() {
-  scrollTo();
-})();
+//   (function() {
+//     scrollTo();
+//   }
+// )();
 
 function scrollTo() {
   const links = document.querySelectorAll(".scroll");
